52d21fdc724216f2d78dcf22e61114da8116c825
Nicholas Njeru <nicholasnjeru917@gmail.com>
2022-11-09 17:17:26 +0300

    YM-7 added a transaction modal which display the details about the car

--- src/App/components/Admin/SellCar/index.jsx
+++ src/App/components/Admin/SellCar/index.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 function SellCar() {
   const [sales, setSales] = useState({});
+  const [showModal, setShowModal] = useState(false);
 
   function handleChange(e) {
     const value = e.target.value;
@@ -10,8 +11,24 @@ function SellCar() {
   }
 
   function allData() {
+    setShowModal(true);
     console.log(sales);
   }
+
+  function handleTransaction() {
+    console.log(car);
+    console.log(sales);
+    setShowModal(false);
+  }
+
+  const car = {
+    category: "Subaru",
+    model: "impreza",
+    price: 1200000,
+    transmission: "manual",
+    fuel: "petrol",
+  };
+
   return (
     <div className="bg-gradient-to-r from-violet-500 to-fuchsia-500 w-screeen h-screen">
       <h1 className="text-center p-auto">Sell Car</h1>
@@ -40,21 +57,86 @@ function SellCar() {
             onChange={(e) => handleChange(e)}
             className="rounded-md"
             type="text"
-            name="car_id"
+            name="car_model"
           />
         </div>
         <div className="flex flex-col">
           <button
-            onClick={allData}
-            className="bg-blue-700 hover:bg-blue-800 w-36 text-white rounded-md"
+            className="bg-sky-500 text-white active:bg-sky-600 font-bold  text-xs px-3 py-3 rounded-md shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
             type="button"
-            data-modal-toggle="popup-modal">
-            view transaction
+            onClick={allData}>
+            Process Transaction
           </button>
         </div>
       </form>
-
-      
+      {showModal ? (
+        <>
+          <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none mx-4 ">
+            <div className="relative sm:w-3/4 mx-auto">
+              {/*content*/}
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col sm:h-3/4 bg-white outline-none focus:outline-none">
+                <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+                  <h1 className="text-center">Transaction details</h1>
+                </div>
+                {/*body*/}
+                <div className="relative flex-auto p-3 p-auto m-auto">
+                  <p className="text-dark">
+                    Customer Name:{" "}
+                    <span className="text-orange-500 border-2 border-gray-200 rounded-md">
+                      {sales.customer_name}
+                    </span>
+                  </p>
+                  <p className="text-dark">
+                    Customer Email:{" "}
+                    <span className="text-orange-500 border-2 border-gray-200 rounded-md">
+                      {sales.customer_email}
+                    </span>
+                  </p>
+                  <p className="text-dark">
+                    Car Category:{" "}
+                    <span className="text-orange-500 border-2 border-gray-200 rounded-md">
+                      {car.category}
+                    </span>
+                  </p>
+                  <p className="text-dark">
+                    car transmission:{" "}
+                    <span className="text-orange-500 border-2 border-gray-200 rounded-md">
+                      {car.transmission}
+                    </span>
+                  </p>
+                  <p className="text-dark">
+                    Car Model:{" "}
+                    <span className="text-orange-500 border-2 border-gray-200 rounded-md">
+                      {car.model}
+                    </span>
+                  </p>
+                  <p className="text-dark">
+                    Car Price:{" "}
+                    <span className="text-green-500 border-2 p5 border-gray-200 rounded-md">
+                      {car.price}
+                    </span>
+                  </p>
+                </div>
+                <div className="flex items-center justify-around p-2 border-t border-solid border-slate-200 rounded-b">
+                  <button
+                    className="text-red-500 bg-red-200 background-transparent px-6 py-2 text-xs rounded p-1 outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={() => setShowModal(false)}>
+                    Cancel
+                  </button>
+                  <button
+                    className="bg-emerald-500 text-white active:bg-emerald-600 uppercase text-xs p-1 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={handleTransaction}>
+                    complete
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+        </>
+      ) : null}
     </div>
   );
 }

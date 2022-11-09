2b37ca77773b5ddcd5376dff9ae3ed246e2c1bc5
Timothy <lemayiant4@gmail.com>
2022-11-09 14:19:25 +0300

    YM-8 admin viewcomponent structured

--- src/App/components/Admin/index.jsx
+++ src/App/components/Admin/index.jsx
@@ -2,8 +2,19 @@ import React from 'react'
 
 function Admin() {
   return (
-    <div><h1>this is admin</h1></div>
-  )
+    <div className="bg-white" >
+    <h1 className="text-center text-7xl	font-serif tracking-widest	non-italic	">All Cars</h1>
+  <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+    <h2 className="sr-only">Products</h2>
+
+    <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+      {products.map((product) => { 
+        return <CarStructure key={product.id} product={product} />
+      })}
+    </div>
+  </div>
+</div>
+    )
 }
 
 export default Admin

a814c90881279643c716c5a57ad46557ba2a9f29
Nicholas Njeru <nicholasnjeru917@gmail.com>
2022-11-09 22:14:33 +0300

    YM-7 changed the page color styling of the page

--- src/App/components/Admin/SellCar/index.jsx
+++ src/App/components/Admin/SellCar/index.jsx
@@ -30,9 +30,9 @@ function SellCar() {
   };
 
   return (
-    <div className="bg-gradient-to-r from-violet-500 to-fuchsia-500 w-screeen h-screen">
+    <div className="bg-hero w-screeen h-screen">
       <h1 className="text-center p-auto">Sell Car</h1>
-      <form className="p-2 p-auto m-auto grid sm:grid-cols-1 sm:w-2/3">
+      <form className="p-2 p-auto m-auto grid sm:grid-cols-1 sm:w-2/3 text-black">
         <div className="flex flex-col">
           <label>Buyer Name</label>
           <input
@@ -62,7 +62,7 @@ function SellCar() {
         </div>
         <div className="flex flex-col">
           <button
-            className="bg-sky-500 text-white active:bg-sky-600 font-bold  text-xs px-3 py-3 rounded-md shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+            className="bg-black text-white active:bg-sky-600 font-bold  text-xs px-3 py-3 rounded-md shadow hover:bg-gray-400 outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
             type="button"
             onClick={allData}>
             Process Transaction
@@ -71,51 +71,58 @@ function SellCar() {
       </form>
       {showModal ? (
         <>
-          <div className="justify-center bg  items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none mx-4 ">
+          <div className="justify-center  bg-theme items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none mx-4 ">
             <div className="relative sm:w-3/4 mx-auto">
               {/*content*/}
-              <div className="border-0 sm:rounded-lg shadow-lg relative flex flex-col sm:h-3/4 bg-purple-300 w-screen sm:w-full  outline-none focus:outline-none">
+              <div className="border-0 sm:rounded-lg shadow-lg relative flex flex-col sm:h-3/4 bg-black w-screen sm:w-full  outline-none focus:outline-none">
                 <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
                   <h1 className="text-center">Transaction details</h1>
                 </div>
                 {/*body*/}
-                <div className="relative flex-auto p-3 p-auto m-auto">
-                  <p className="text-dark">
+                <div className="relative flex-row flex-auto p-3  p-auto m-auto">
+                  <div className="text-white bg-theme flex flex-row justify-around">
+                    <span className="text-white">
                     Customer Name:
-                    <span className="text-orange-500 border-2 border-gray-200 rounded-md">
+                    </span>
+                    <span className="text-white border-2 border-gray-200 rounded-md">
                       {sales.customer_name}
                     </span>
-                  </p>
-                  <p className="text-dark">
+                  </div>
+
+                  <div className="text-white bg-theme flex flex-row justify-around pr-1">
+                    <span className="text-white pr-1">
                     Customer Email:
-                    <span className="text-orange-500 border-2 border-gray-200 rounded-md">
-                      {sales.customer_email}
                     </span>
-                  </p>
-                  <p className="text-dark">
+                    <span className="text-white border-2 border-gray-200 rounded-md">
+                    {sales.customer_email}
+                    </span>
+                  </div>
+                  <div className="text-white bg-theme flex flex-row justify-around pr-1">
+                    <span className="text-white pr-1">
                     Car Category:
-                    <span className="text-orange-500 border-2 border-gray-200 rounded-md">
+                    </span>
+                    <span className="text-white border-2 border-gray-200 rounded-md">
                       {car.category}
                     </span>
-                  </p>
-                  <p className="text-dark">
+                  </div>
+                  <div className="text-white bg-theme flex flex-row justify-around pr-1">
                     car transmission:
-                    <span className="text-orange-500 border-2 border-gray-200 rounded-md">
+                    <span className="text-white border-2 border-gray-200 rounded-md">
                       {car.transmission}
                     </span>
-                  </p>
-                  <p className="text-dark">
+                  </div>
+                  <div className="text-white bg-theme flex flex-row justify-around pr-1">
                     Car Model:
-                    <span className="text-orange-500 border-2 border-gray-200 rounded-md">
+                    <span className="text-white border-2 border-gray-200 rounded-md">
                       {car.model}
                     </span>
-                  </p>
-                  <p className="text-dark">
+                  </div>
+                  <div className="text-white bg-theme flex flex-row justify-around pr-1">
                     Car Price:
-                    <span className="text-green-500 border-2 p5 border-gray-200 rounded-md">
+                    <span className="text-white border-2 border-gray-200 rounded-md">
                       {car.price}
                     </span>
-                  </p>
+                  </div>
                 </div>
                 <div className="flex items-center justify-around p-2 border-t border-solid border-slate-200 rounded-b">
                   <button
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'hero': "url('https://images.pexels.com/photos/4194857/pexels-photo-4194857.jpeg?auto=compress&cs=tinysrgb&w=1600')",
+      },
+    },
   },
   plugins: [],
 }

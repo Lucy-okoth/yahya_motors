f6387f1767d54bc1cfffe745c0deb9cf880a3d6d
Peter <peterkilungya1@gmail.com>
2022-11-10 09:00:24 +0300

    YM-9 added some input fields

--- src/App/components/Admin/AddCar/index.jsx
+++ src/App/components/Admin/AddCar/index.jsx
@@ -12,7 +12,7 @@ const [carData,setCarData] = useState({})
   }
   const models = "bg-violet-300 rounded-md hover:bg-violet-600 text-red-600";
   return (
-    <div className="h-screen bg-gray-400 w-screen">
+    <div className="h-full bg-gray-400 w-screen">
     <h1 className="text-center p-auto">Add Car</h1>
     <form className="p-2 p-auto m-auto grid sm:grid-cols-1 w-2/3">
       <div className="flex flex-col">
@@ -28,16 +28,40 @@ const [carData,setCarData] = useState({})
         <input onChange={(e) => handleInput(e)}  className="rounded-md" type="text" name="seats" />
       </div>
       <div className="flex flex-col">
-        <label>price</label>
+        <label>Price</label>
         <input onChange={(e) => handleInput(e)}  className="rounded-md" type="text" name="price"/>
+      </div>
+      <div className="flex flex-col">
+        <label>Quantity</label>
+        <input onChange={(e) => handleInput(e)}  className="rounded-md" type="text" name="quantity"/>
+      </div>
+      <div className="flex flex-col">
+        <label>Fuel type</label>
+        <input onChange={(e) => handleInput(e)}  className="rounded-md" type="text" name="fuel_type"/>
+      </div>
+      <div className="flex flex-col">
+        <label>Year</label>
+        <input onChange={(e) => handleInput(e)}  className="rounded-md" type="text" name="year"/>
       </div>
       <div className="flex flex-col mb-4">
         <label>Transmission</label>
-        <input onChange={(e) => handleInput(e)}  className='rounded -md' name="description"/>
+        <input onChange={(e) => handleInput(e)}  className='rounded -md' name="transmission"/>
       </div>
       <div className="flex flex-col mb-4">
-        <label>image url</label>
-        <input onChange={(e) => handleInput(e)}  className='rounded -md' name="description"/>
+        <label>Front url</label>
+        <input onChange={(e) => handleInput(e)}  className='rounded -md' name="front_url"/>
+      </div>
+      <div className="flex flex-col mb-4">
+        <label>Back url</label>
+        <input onChange={(e) => handleInput(e)}  className='rounded -md' name="back_url"/>
+      </div>
+      <div className="flex flex-col mb-4">
+        <label>Side url</label>
+        <input onChange={(e) => handleInput(e)}  className='rounded -md' name="side_url"/>
+      </div>
+      <div className="flex flex-col mb-4">
+        <label>Inside url</label>
+        <input onChange={(e) => handleInput(e)}  className='rounded -md' name="inside_url"/>
       </div>
       <div className="flex flex-col">
         <button

4d6ea9c3041234e711c0ff163d86eb810a19033b
Timothy <lemayiant4@gmail.com>
2022-11-10 18:42:22 +0300

    YM-27-done

--- src/App/components/CarStructure/index.jsx
+++ src/App/components/CarStructure/index.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function CarStructure({car}) {
   return (
-  <a key={car.id} href={car.id} className="group">
+  <Link key={car.id} to="${car.id}" className="group">
     <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
       <img
         src={car.front_url}
@@ -12,7 +13,7 @@ function CarStructure({car}) {
     </div>
     <h3 className="mt-4 text-sm text-gray-700">{car.model}</h3>
     <p className="mt-1 text-lg font-medium text-gray-900">{car.price}</p>
-  </a>
+  </Link>
   )
 }
 

35e4ab2506ab78d3624356cca16268a33cad2881
Nicholas Njeru <nicholasnjeru917@gmail.com>
2022-11-11 10:05:51 +0300

    YM-38 updates on home page

--- src/App/components/Home/index.jsx
+++ src/App/components/Home/index.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 
 function Home() {
   return (
-    <div className='w-full h-[90vh] top[90px]'>
+    <div className='w-screen'>
       <img className='w-full h-full' src="https://www.nissanusa.com/content/dam/Nissan/us/vehicles/gtr/2021/overview/2021-nissan-gtr-supercar.jpg"alt="logo"/>
       <div className='w-full h-[90%] flex flex-col justify-center items-center text-white px-4 text-center'>
-        <h1 className='text-5xl font-bold text-white'>
+        <h1 className='text-5xl font-bold text-black'>
           DRIVING TOMORROW TODAY
         </h1>
         <h1 className='text-xl py-4'>

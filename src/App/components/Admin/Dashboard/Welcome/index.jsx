695db80de25786495fbce5125f7e4c61c00c5826
Nicholas Njeru <nicholasnjeru917@gmail.com>
2022-11-14 11:26:57 +0300

    YM-47 this are some updates

---
+++ src/App/components/Admin/Dashboard/Welcome/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Welcome() {
+  return (
+    <>
+    <div className='p-5 bg-slate-200 rounded-md'>Welcome Admin</div>
+    <div className='grid sm:grid'>
+
+
+    </div>
+    
+    </>
+  )
+}
+
+export default Welcome
